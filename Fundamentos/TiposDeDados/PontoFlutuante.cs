8c7fe667ef135cdff7c3a91daa89787d4715f4e7
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-08-30 10:55:51 -0300

    criado exemplo dos pontos flutuantes

---
+++ Fundamentos/TiposDeDados/PontoFlutuante.cs
@@ -0,0 +1,18 @@
+﻿namespace Fundamentos.TiposDeDados;
+
+public static class PontoFlutuante
+{
+    public static void Executar()
+    {
+        // Tipos de ponto flutuante
+        Console.WriteLine("Tipos de ponto flutuante");
+        float valorFloat = 3.14f; // 32 bits
+        double valorDouble = 3.14; // 64 bits
+        decimal valorDecimal = 3.14m; // 128 bits
+
+        // Exemplo de uso
+        Console.WriteLine($"Valor de float: {valorFloat}");
+        Console.WriteLine($"Valor de double: {valorDouble}");
+        Console.WriteLine($"Valor de decimal: {valorDecimal}");
+    }
+}
